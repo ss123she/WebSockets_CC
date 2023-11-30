ce1b9a3839101f9f33716b0202469b6271375d6b
ss123she <gamegames8293@gmail.com>
2023-11-30 18:59:47 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const ws = require("ws")
 const wss = new ws.Server({port:8081})
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+const server = http.createServer(app);
+
+const PORT = 8081;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 wss.on("connection",ws=>{
     console.log("connection!")
