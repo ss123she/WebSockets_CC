445628c636b7836a9834e7c758db38c7c375cdfd
ss123she <gamegames8293@gmail.com>
2023-12-02 01:01:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-  const setup = {port:8000}
-  const express = require ('express');
-  const app = express ();
+const setup = { port: 8000 };
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(express.static('public'));
+// Используйте body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-  app.get('/computer', (req, res) => {
+app.get('/computer', (req, res) => {
     res.sendFile(__dirname + '/public/computer.html');
-  });
+});
 
-  app.post('/submit', (req, res) => {
+app.post('/submit', (req, res) => {
     const data = req.body.data;
-
     console.log(`Полученные данные: ${data}`);
-
     res.json({ message: 'Данные успешно отправлены!' });
 });
 
-  app.listen(setup.port, () => {
+app.listen(setup.port, () => {
     console.log('Сервер запущен: порт %s', setup.port);
-  });
+});
