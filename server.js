26977ed4d00b9ab742a5b029923cf99e5877b4ae
ss123she <gamegames8293@gmail.com>
2023-12-02 20:19:22 +0300

    a

--- server.js
+++ server.js
@@ -2,7 +2,23 @@ const setup = { port: 8000 };
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const ws = require('ws');
+const wsServer = new ws.Server({port: 9000});
 
+users = 0;
+wsServer.on('connection', function() {
+    console.log("пользователь " + users + " подключился");
+    users++;
+    console.log("current users: " + users);
+});
+wsServer.on('error', function() {
+    console.error("Error with WebSocket connection")
+});
+wsServer.on('close', function() {
+    console.log("WebSocket connection closed")
+    users = users - 1;
+    console.log("current users: " + users);
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static('public'));
