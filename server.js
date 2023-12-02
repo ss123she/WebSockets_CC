c221d6cb97c187e49af1fafa25afb9dadb46fcd4
ss123she <gamegames8293@gmail.com>
2023-12-02 13:12:44 +0300

    s

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const setup = { port: 8000 };
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 
-// Используйте body-parser
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.post('/computer', (req, res) => {
-    const firstName = req.body.fname;
-    const lastName = req.body.lname;
-
+    console.log(`Full name is:${req.body.fname} ${req.body.lname}.`);
     res.json({ status: 'success', message: 'Data received successfully.' });
-    console.log("recieved " + firstName + " " + lastName)
 });
 
 app.get('/computer', (req, res) => {
     res.sendFile(__dirname + '/public/computer/computer.html');
 });
+
 app.get('/gta', (req, res) => {
     res.sendFile(__dirname + '/public/gta/gta.html');
 });
