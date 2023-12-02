4eac15fa72a87401fed61ed1c68d402d3fc716b5
ss123she <gamegames8293@gmail.com>
2023-12-02 03:17:38 +0300

    ok

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const setup = { port: 8000 };
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const WebSocket = require('ws');
+const http = require('http');
 
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
 // Используйте body-parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/computer', (req, res) => {
-    res.sendFile(__dirname + '/public/computer.html');
+    res.sendFile(__dirname + '/public/computer/computer.html');
+});
+app.get('/gta', (req, res) => {
+    res.sendFile(__dirname + '/public/gta/gta.html');
 });
 
 app.post('/submit', (req, res) => {
@@ -20,3 +27,7 @@ app.post('/submit', (req, res) => {
 app.listen(setup.port, () => {
     console.log('Сервер запущен: порт %s', setup.port);
 });
+
+server.listen(3000, () => {
+    console.log('WebSocket server listening on http://localhost:3000');
+  });
