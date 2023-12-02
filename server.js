0e3a13bb53e6380ecbfdabf228145128a52f5208
ss123she <gamegames8293@gmail.com>
2023-12-02 18:55:35 +0300

    a

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-
-app.post('/computer', (req, res) => {
-    console.log(`Full name is: ${req.body.fname} ${req.body.lname}.`);
-});
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.get('/computer', (req, res) => {
     res.sendFile(__dirname + '/public/computer/computer.html');
-});
+});+
 
 app.get('/gta', (req, res) => {
     res.sendFile(__dirname + '/public/gta/gta.html');
 });
 
+app.post('/run-script', (req, res) => {
+    console.log('succes');
+});
 app.listen(setup.port, () => {
     console.log('Сервер запущен: порт %s', setup.port);
 });
